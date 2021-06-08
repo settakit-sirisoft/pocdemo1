9e67f71917727d6da9b2ddc9ea41e6a08ee75cad
Settakit Thasri <settakit@sirisoft.co.th>
2021-06-08 12:30:15 +0700

    test

--- pocdemo1.go
+++ pocdemo1.go
@@ -17,11 +17,48 @@ type FooResponse struct {
 	Bar string `json:"bar"`
 }
 
+type UserRequest struct {
+	XMLName    xml.Name `xml:"get-user-request"`
+	Permission int      `json:"permission"`
+}
+
+type UserResponse struct {
+	UID      string
+	Username string
+}
+
+type StatusResponse struct {
+	Status  string
+	Message string
+	Data    []UserResponse
+}
+
 func PrintRequest(rq FooRequest) FooResponse {
 
 	resp := FooResponse{
 		Foo: "bar",
 		Bar: "hello " + rq.Foo,
+	}
+
+	fmt.Println(resp)
+
+	return resp
+}
+
+func GetUserRequest(rq UserRequest) StatusResponse {
+
+	if rq.Permission != 0 {
+		return StatusResponse{Status: "fail", Message: "permission denied"}
+	}
+
+	resp := StatusResponse{
+		Status:  "success",
+		Message: "success",
+		Data: []UserResponse{
+			{UID: "1111", Username: "TEST1"},
+			{UID: "2222", Username: "TEST2"},
+			{UID: "3333", Username: "TEST3"},
+		},
 	}
 
 	fmt.Println(resp)
