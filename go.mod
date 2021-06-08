551e3b95903ea87fa7ab85befad92f43204c0e87
Settakit Thasri <settakit@sirisoft.co.th>
2021-06-08 11:26:58 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pocdemo1
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,30 @@
+package pocdemo1
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+// FooRequest a simple request
+type FooRequest struct {
+	XMLName xml.Name `xml:"fooRequest"`
+	Foo     string   `json:"foo"`
+}
+
+// FooResponse a simple response
+type FooResponse struct {
+	Foo string `json:"foo"`
+	Bar string `json:"bar"`
+}
+
+func PrintRequest(rq *FooRequest) FooResponse {
+
+	resp := FooResponse{
+		Foo: "bar",
+		Bar: "hello " + rq.Foo,
+	}
+
+	fmt.Println(resp)
+
+	return resp
+}
